e9fef2bd9b9294f8c21a24562c84013f584f3790
te22e-Olsson-Anton <anton.olsson3@elev.ga.ntig.se>
2025-04-01 10:44:31 +0200

    små ändringar i koden och lagt till så att den loopar. Ifall man skriver fel användarnamn eller lösenord så kan man logga in utan att starta om programmet

--- src/Main.java
+++ src/Main.java
@@ -4,58 +4,84 @@ public class Main {
     public static void main(String[] args) {
         Scanner tangentbord = new Scanner(System.in);
 
-        System.out.println("1. För att logga in som ANVÄNDARE");
-        System.out.println("2. För att logga in som ADMIN");
+        while (true) {
+            System.out.println("1. För att logga in som ANVÄNDARE");
+            System.out.println("2. För att logga in som ADMIN");
+            System.out.println("3. Avsluta programmet");
 
-        int val = tangentbord.nextInt();
-        tangentbord.nextLine(); // Rensar bufferten efter `nextInt()`
+            int val = tangentbord.nextInt();
+            tangentbord.nextLine();
 
-        if (val == 1) {
-            loginUser(tangentbord); // Skicka scanner-objektet
-        } else if (val == 2) {
-            loginAdmin(tangentbord); // Skicka scanner-objektet
-        } else {
-            System.out.println("Felaktigt val. Vänligen ange 1 eller 2.");
+            if (val == 1)
+            {
+                if (loginUser(tangentbord)) break;
+            }
+            else if (val == 2)
+            {
+                if (loginAdmin(tangentbord)) break;
+            }
+            else if (val == 3) {
+                System.out.println("Programmet avslutas...");
+                break;
+            }
+            else
+            {
+                System.out.println("Felaktigt val. Vänligen ange 1, 2 eller 3.");
+            }
         }
 
-        tangentbord.close(); // Stänger Scanner när vi är klara
+        tangentbord.close();
     }
 
-    public static void loginUser(Scanner input) {
-        System.out.println("\nVälkommen till (namnet på banken)\n");
-        System.out.println("Ange användarnamn och lösenord för att logga in som ANVÄNDARE");
+    public static boolean loginUser(Scanner input) {
+        while (true)
+        {
+            System.out.println("\nVälkommen till (namnet på banken)\n");
+            System.out.println("Ange användarnamn och lösenord för att logga in som ANVÄNDARE");
 
-        System.out.print("Användarnamn: ");
-        String username = input.nextLine();
+            System.out.print("Användarnamn: ");
+            String username = input.nextLine();
 
-        System.out.print("Lösenord: ");
-        String password = input.nextLine();
+            System.out.print("Lösenord: ");
+            String password = input.nextLine();
 
-        User login = new User(username, password);
+            User login = new User(username, password);
 
-        if (login.checkPassword()) {
-            System.out.println("Du är inloggad som användare");
-        } else {
-            System.out.println("Användarnamnet eller lösenordet är felaktigt");
+            if (login.checkPassword())
+            {
+                System.out.println("Du är inloggad som användare");
+                return true;
+            }
+            else
+            {
+                System.out.println("Användarnamnet eller lösenordet är felaktigt. Försök igen.");
+            }
         }
     }
 
-    public static void loginAdmin(Scanner input) {
-        System.out.println("\nVälkommen till (namnet på banken)\n");
-        System.out.println("Ange admin-användarnamn och lösenord för att logga in som ADMIN");
+    public static boolean loginAdmin(Scanner input) {
+        while (true)
+        {
+            System.out.println("\nVälkommen till (namnet på banken)\n");
+            System.out.println("Ange admin-användarnamn och lösenord för att logga in som ADMIN");
 
-        System.out.print("Användarnamn: ");
-        String adminUsername = input.nextLine();
+            System.out.print("Användarnamn: ");
+            String adminUsername = input.nextLine();
 
-        System.out.print("Lösenord: ");
-        String adminPassword = input.nextLine();
+            System.out.print("Lösenord: ");
+            String adminPassword = input.nextLine();
 
-        Admin adminLogin = new Admin(adminUsername, adminPassword);
+            Admin adminLogin = new Admin(adminUsername, adminPassword);
 
-        if (adminLogin.adminCheckPassword()) {
-            System.out.println("Du är inloggad som ADMIN");
-        } else {
-            System.out.println("Användarnamnet eller lösenordet är felaktigt");
+            if (adminLogin.adminCheckPassword())
+            {
+                System.out.println("Du är inloggad som ADMIN");
+                return true;
+            }
+            else
+            {
+                System.out.println("Användarnamnet eller lösenordet är felaktigt. Försök igen.");
+            }
         }
     }
 }
